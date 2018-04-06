63ce136b9e2e43fdb884099d7e60c7bf4636b40c
Gabriel Dunn <gabe.dunn@shaw.ca>
2018-04-06 00:12:53 -0600

    add tags command

---
+++ src/commands/tags.js
@@ -0,0 +1,45 @@
+import { Command } from 'discord-akairo'
+
+import { msgDeleteTime, prefix } from '../config'
+import colours from '../colours'
+
+import tags from '../tags'
+
+export default class TagCommand extends Command {
+  constructor () {
+    super('tags', {
+      aliases: ['tags', 'taglist']
+    })
+  }
+
+  // noinspection JSMethodCanBeStatic
+  async exec (message) {
+    const fields = []
+
+    for (const [key, val] of Object.entries(tags)) {
+      fields.push({name: val.title, value: `${prefix}tag ${key}`})
+    }
+
+    if (fields.length === 0) {
+      fields.push({
+        name: 'No Tags',
+        value: 'There are currently no tags added.'
+      })
+    }
+
+    const sent = await message.util.send({
+      embed: {
+        title: 'List of Tags',
+        color: colours.blue,
+        fields,
+        author: {
+          name: message.member.user.username,
+          icon_url: message.member.user.avatarURL
+        }
+      }
+    })
+    setTimeout(() => {
+      sent.delete(1)
+    }, msgDeleteTime * 1000)
+  }
+}
--- src/config.js
+++ src/config.js
@@ -3,3 +3,4 @@ require('dotenv').config()
 export const botToken = process.env.BOT_TOKEN
 export const ownerID = process.env.OWNER_ID
 export const prefix = process.env.PREFIX
+export const msgDeleteTime = process.env.MSG_DELETE_TIME
