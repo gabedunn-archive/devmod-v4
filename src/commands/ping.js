69913649cd542842aced72cd5fbe8eeb4d2134fc
Gabriel Dunn <gabe.dunn@shaw.ca>
2018-04-06 19:02:38 -0600

    make ping async

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -10,25 +10,24 @@ export default class PingCommand extends Command {
     })
   }
 
-  exec (message) {
+  async exec (message) {
     const embed = {
       title: 'Pong!',
       color: colours.blue
     }
-    return message.util.send({embed}).then(sent => {
-      const timeDiff = (sent.editedAt || sent.createdAt) -
-        (message.editedAt || message.createdAt)
-      embed.fields = [
-        {
-          name: 'Round Trip Time:',
-          value: `${timeDiff}ms.`
-        },
-        {
-          name: 'Ping:',
-          value: `${Math.round(this.client.ping)}ms.`
-        }
-      ]
-      return message.util.send({embed})
-    })
+    const sent = await message.util.send({embed})
+    const timeDiff = (sent.editedAt || sent.createdAt) -
+      (message.editedAt || message.createdAt)
+    embed.fields = [
+      {
+        name: 'Round Trip Time:',
+        value: `${timeDiff}ms.`
+      },
+      {
+        name: 'Ping:',
+        value: `${Math.round(this.client.ping)}ms.`
+      }
+    ]
+    return message.util.send({embed})
   }
 }
