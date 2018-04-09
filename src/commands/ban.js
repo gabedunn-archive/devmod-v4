ab21bc89d045d9ff5ac2dd2c61aa603db3ae6d6c
Gabriel Dunn <gabe.dunn@shaw.ca>
2018-04-08 21:52:06 -0600

    add try/catch blocks to ban command

--- src/commands/ban.js
+++ src/commands/ban.js
@@ -32,78 +32,109 @@ export default class BanCommand extends Command {
     })
   }
 
-  // TODO: add proper try/catch for error handling
   // noinspection JSMethodCanBeStatic
   async exec (message, args) {
-    if (!args.member) {
-      await message.react('❌')
-      const embed = errorMessage('Member Not Found', 'No member found with' +
-        ' that name.')
-      return message.util.send({embed})
-    }
-    if (!args.reason) {
-      await message.react('❌')
-      const embed = errorMessage('Reason Not Specified', 'Please specify a' +
-        ' reason for the ban.')
-      return message.util.send({embed})
-    }
-    if (args.member.permissions.has('KICK_MEMBERS')) {
-      await message.react('❌')
-      const embed = errorMessage('Can\'t Ban Member', 'You are not allowed' +
-        ' to ban that member.')
-      return message.util.send({embed})
-    }
-
-    await message.delete(1)
-
-    const user = args.member.user
-
-    const time = args.rm ? 7 : 0
-
-    await user.send({
-      embed: {
-        title: `You have been banned from ${message.guild.name}.`,
-        color: colours.red,
-        thumbnail: {
-          url: message.guild.iconURL
-        },
-        fields: [
-          {
-            name: 'Reason:',
-            value: args.reason
-          }],
-        footer: {
-          text: `Your messages from the past ${time} days have been deleted.`
+    try {
+      try {
+        if (!args.member) {
+          await message.react('❌')
+          const embed = errorMessage('Member Not Found',
+            'No member found with that name.')
+          return message.util.send({embed})
         }
+      } catch (e) {
+        console.log(`Error sending member not found message: ${e.message}`)
+        return null
       }
-    })
-    message.guild.channels.find('name', channels.ban).send({
-      embed: {
-        color: colours.red,
-        title: 'Ban',
-        description: 'User has been banned',
-        author: {
-          name: message.member.user.username,
-          icon_url: message.member.user.avatarURL
-        },
-        fields: [
-          {
-            name: 'Reason:',
-            value: args.reason
-          }],
-        footer: {
-          icon_url: user.avatarURL,
-          text: `${user.tag}'s messages from the past ${time} days have been deleted.`
-        },
-        timestamp: new Date()
+      try {
+        if (!args.reason) {
+          await message.react('❌')
+          const embed = errorMessage('Reason Not Specified',
+            'Please specify a reason for the ban.')
+          return message.util.send({embed})
+        }
+      } catch (e) {
+        console.log(`Error sending reason not specified message: ${e.message}`)
+        return null
       }
-    })
-    await args.member.ban({
-      days: time,
-      reason: args.reason
-    })
+      try {
+        if (args.member.permissions.has('KICK_MEMBERS')) {
+          await message.react('❌')
+          const embed = errorMessage(
+            'Can\'t Ban Member', 'You are not allowed' +
+            ' to ban that member.')
+          return message.util.send({embed})
+        }
+      } catch (e) {
+        console.log(`Error sending can't ban member message: ${e.messsage}`)
+        return null
+      }
 
-    const db = await sqlite.open(dbFile)
-    return db.run('DELETE FROM `warnings` WHERE `discord_id` = ?', user.id)
+      try {
+        await message.delete(1)
+
+        const user = args.member.user
+
+        const time = args.rm ? 7 : 0
+
+        await user.send({
+          embed: {
+            title: `You have been banned from ${message.guild.name}.`,
+            color: colours.red,
+            thumbnail: {
+              url: message.guild.iconURL
+            },
+            fields: [
+              {
+                name: 'Reason:',
+                value: args.reason
+              }],
+            footer: {
+              text: `Your messages from the past ${time} days have been deleted.`
+            }
+          }
+        })
+        await message.guild.channels.find('name', channels.ban).send({
+          embed: {
+            color: colours.red,
+            title: 'Ban',
+            description: 'User has been banned',
+            author: {
+              name: message.member.user.username,
+              icon_url: message.member.user.avatarURL
+            },
+            fields: [
+              {
+                name: 'Reason:',
+                value: args.reason
+              }],
+            footer: {
+              icon_url: user.avatarURL,
+              text: `${user.tag}'s messages from the past ${time} days have been deleted.`
+            },
+            timestamp: new Date()
+          }
+        })
+        await args.member.ban({
+          days: time,
+          reason: args.reason
+        })
+      } catch (e) {
+        console.log(`Error sending messages and banning user: ${e.messsage}`)
+        return null
+      }
+
+      try {
+        const db = await sqlite.open(dbFile)
+        return db.run('DELETE FROM `warnings` WHERE `discord_id` = ?', user.id)
+      } catch (e) {
+        console.log(`Error deleting entries from database: ${e.messsage}`)
+        return null
+      }
+    } catch (e) {
+      console.log(`Ban command failed: ${e.message}`)
+      console.log(`Line Number: ${e.lineNumber}`)
+      return null
+    }
   }
 }
