2725262c5fca1a1c97af0cf7e02b8afdfb28ecb2
Gabriel Dunn <gabe.dunn@shaw.ca>
2018-04-10 01:17:16 -0600

    add easter egg statuses

--- index.js
+++ index.js
@@ -12,8 +12,6 @@ const client = new AkairoClient({
   commandUtil: true,
   commandUtilLifetime: 600000,
   commandDirectory: join(__dirname, 'src', 'commands')
-  // inhibitorDirectory: join(__dirname, 'src', 'inhibitors')
-  // listenerDirectory: join(__dirname, 'src', 'listeners')
 })
 
 const run = async () => {
@@ -22,9 +20,23 @@ const run = async () => {
   client.user.setActivity('.help')
 
   const activities = [
-    'Counting GBP...',
-    'Trying to unban SWAT SEAL...',
-    'Spamming the DMs of a random user...'
+    'Counting your Good Boye Points.',
+    'Trying to unban SWAT SEAL.',
+    'Spamming the DMs of a random user.',
+    'Compiling.',
+    'Having a snack.',
+    'uncaughtException',
+    'Shitposting in #off-topic-chat.',
+    'Admiring egg.',
+    'Trying to exit vim.',
+    'BEING A HUMAN.',
+    '10011001101111100000011011110',
+    'Hacking the FBI.',
+    'Serving NaN users.',
+    'on a Christian Server',
+    'et ur brokli',
+    'this chat gets weird fasy',
+    'https://i.imgur.com/BVRHZzg.png'
   ]
 
   setInterval(async () => {
