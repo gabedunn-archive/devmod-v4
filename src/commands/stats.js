8d72ff0782839159ca44b7988ab99722810d136d
Gabriel Dunn <gabe.dunn@shaw.ca>
2018-04-12 14:53:12 -0600

    add stats command

---
+++ src/commands/stats.js
@@ -0,0 +1,48 @@
+import { Command } from 'discord-akairo'
+import moment from 'moment'
+
+import colours from '../colours'
+
+export default class StatsCommand extends Command {
+  constructor () {
+    super('stats', {
+      aliases: ['stats', 'statistics'],
+      category: 'util',
+      description: 'Shows some statistics about the server.'
+    })
+  }
+
+  // noinspection JSMethodCanBeStatic
+  async exec (message) {
+    try {
+      const guild = message.guild
+      const client = message.client
+      const uptime = moment.duration(client.uptime)
+      await message.delete(1)
+      return message.util.send({
+        embed: {
+          title: 'Server Stats',
+          color: colours.blue,
+          fields: [
+            {
+              name: guild.name,
+              value: `Members: ${guild.memberCount}.\n` +
+              `Server was created at: ${moment(guild.createdAt).
+                format('YYYY/M/D')}.\n` +
+              `Num. of channels: ${ message.guild.channels.array().length}.\n` +
+              `Region: ${guild.region}.\n` +
+              `AFK Timeout: ${guild.afkTimeout}s.\n`
+            },
+            {
+              name: 'Bot Information',
+              value: `Uptime: ${uptime.hours()} hours, ${uptime.minutes()} mins, ${uptime.seconds()}s.`
+            }
+          ]
+        }
+      })
+    } catch (e) {
+      console.log(`Error sending stats message: ${e}`)
+      return null
+    }
+  }
+}
