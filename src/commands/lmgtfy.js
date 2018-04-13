5c318da6369625b98f55a03f0a116109f5e3bd27
Gabriel Dunn <gabe.dunn@shaw.ca>
2018-04-12 22:01:26 -0600

    add lmgtfy command

---
+++ src/commands/lmgtfy.js
@@ -0,0 +1,45 @@
+import { Command } from 'discord-akairo'
+import colours from '../colours'
+import { errorMessage } from '../common'
+
+export default class LMGTFYCommand extends Command {
+  constructor () {
+    super('lmgtfy', {
+      aliases: ['lmgtfy'],
+      category: 'assistance',
+      description: 'Sends a LMGTFY link with the specified query.',
+      args: [
+        {
+          id: 'query',
+          match: 'rest'
+        }
+      ]
+    })
+  }
+
+  // noinspection JSMethodCanBeStatic
+  async exec (message, args) {
+    try {
+      if (!args.query) {
+        await message.react('❌')
+        return message.util.send({
+          embed: errorMessage(
+            'No Query Specified', 'You need to specify a query.'
+          )
+        })
+      }
+      await message.delete(1)
+      return message.util.send({
+        embed: {
+          title: 'LMGTFY',
+          color: colours.blue,
+          url: `https://lmgtfy.com/?q=${args.query.replace(/\s/g, '+')}`,
+          description: 'Here you go!'
+        }
+      })
+    } catch (e) {
+      console.log(`Error sending initial message: ${e}`)
+      return null
+    }
+  }
+}
