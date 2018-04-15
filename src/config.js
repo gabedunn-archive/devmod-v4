a7f5cf693c9519ec96a65a05af4a408cade55352
Gabriel Dunn <gabe.dunn@shaw.ca>
2018-04-15 11:36:25 -0600

    add default values in config.js

--- src/config.js
+++ src/config.js
@@ -5,18 +5,18 @@ dotenv.config()
 
 export const botToken = process.env.BOT_TOKEN
 export const ownerID = process.env.OWNER_ID
-export const prefix = process.env.PREFIX
-export const msgDeleteTime = process.env.MSG_DELETE_TIME
-export const dbFile = join(__dirname, '..', process.env.DB_FILE)
-export const autoBan = process.env.AUTOBAN
-export const autoBanWarns = process.env.AUTOBAN_WARNS
-export const banMsgDelete = process.env.BAN_MSG_DELETE
+export const prefix = process.env.PREFIX || '.'
+export const msgDeleteTime = process.env.MSG_DELETE_TIME || 10
+export const dbFile = join(__dirname, '..', process.env.DB_FILE || 'devmod.sqlite')
+export const autoBan = process.env.AUTOBAN || true
+export const autoBanWarns = process.env.AUTOBAN_WARNS || 3
+export const banMsgDelete = process.env.BAN_MSG_DELETE || 0
 export const channels = {
-  warn: process.env.CHANNEL_LOG_WARN,
-  ban: process.env.CHANNEL_LOG_BAN,
-  report: process.env.CHANNEL_REPORT,
-  roles: process.env.CHANNEL_ROLES
+  warn: process.env.CHANNEL_LOG_WARN || 'warnings',
+  ban: process.env.CHANNEL_LOG_BAN || 'bans',
+  report: process.env.CHANNEL_REPORT || 'reports',
+  roles: process.env.CHANNEL_ROLES || 'roles'
 }
-export const pointEmoji = process.env.POINTS_EMOJI
-export const statusInterval = process.env.STATUS_INTERVAL
-export const pointsTopCount = process.env.POINTS_TOP_COUNT
+export const pointEmoji = process.env.POINTS_EMOJI || 'boye'
+export const statusInterval = process.env.STATUS_INTERVAL || 5
+export const pointsTopCount = process.env.POINTS_TOP_COUNT || 10
