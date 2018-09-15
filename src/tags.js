79c26d9d2c8b583ec5771add6619b818aec69983
DillonSimeone <Dillonsimeone@gmail.com>
2018-09-14 22:28:03 -0700

    Add Flexbox Tag
    I'm not sure if colours.purple will work.
    
    I'm not sure if `[Blah blah blah](www.website.com)` will work in the name fields instead of the value fields. 
    
    If this don't work, kick me and I'll fix it.

--- src/tags.js
+++ src/tags.js
@@ -297,5 +297,30 @@ export default {
         value: 'What is the preferred method of communication? If a developer is interested, how should they contact you? Do NOT give out email addresses or other personally identifiable information.'
       }
     ]
+  },
+  flex: {
+    title: 'Flexbox',
+    color: colours.purple,
+    description: 'The Flexible Box Module, usually referred to as flexbox, was' +
+        'designed as a one-dimensional layout model, and as a method that could' + 
+        'offer space distribution between items in an interface and powerful alignment capabilities.',
+    fields: [
+      {
+        name: '[MDN web docs](https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Flexible_Box_Layout/Basic_Concepts_of_Flexbox)',
+        value: 'Flexbox documents.'
+      },
+      {
+        name: '[Flexbox quickstart guide](https://css-tricks.com/snippets/css/a-guide-to-flexbox/)',
+        value: 'Learn flexbox in minutes! Easy, and powerful!'
+      },
+      {
+        name: '[Flexbox froggy](https://flexboxfroggy.com)',
+        value: 'Get frogs to where they need to be with the magic of flexbox!'
+      },
+      {
+        name: '[Flexbox Defense](http://www.flexboxdefense.com/)',
+        value: 'Blast enemies into nothingness with defensive towers postioned by flexbox'
+      }
+    ]
   }
 }
